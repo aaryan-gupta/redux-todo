d4843e470280ad09cb265db12389c3056cb27ba7
Aaryan Gupta <66556617+aaryan-gupta@users.noreply.github.com>
2021-05-19 15:23:18 +0530

    some changes

--- src/reducers/todoReducers.js
+++ src/reducers/todoReducers.js
@@ -3,7 +3,7 @@ const todoReducers = (state = initialData, action) => {
 	switch (action.type) {
 		case "ADD_TODO":
 			const { id, data } = action.payload
-			return { ...state, list: [...state.list, { id, data }] }
+			if (data.length > 2) return { ...state, list: [...state.list, { id, data }] }
 
 		case "DELETE_TODO":
 			const newList = state.list.filter(item => item.id !== action.id)
